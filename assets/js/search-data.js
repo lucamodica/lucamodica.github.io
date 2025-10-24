2b0b62429f1b5d82d4bfaf66a82dd1733bedd12d
lucamodica <luca.modica55@gmail.com>
2025-10-24 23:37:13 +0000

    Deploying to gh-pages from @ lucamodica/lucamodica.github.io@cc7f1a4e7ae90d2ad69a9464fda92bdce8314ffc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -501,14 +501,21 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%6C%75%63%61%6D%6F%64%69%63%61.%62%75%73%69%6E%65%73%73@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-kaggle',
+        title: 'Kaggle',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://www.kaggle.com/lucamodica", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/lucamodica", "_blank");
         },
       },{
         id: 'social-rss',
@@ -518,18 +525,11 @@ ninja.data = [{
           window.open("/feed.xml", "_blank");
         },
       },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
+        id: 'social-x',
+        title: 'X',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://twitter.com/lu_modica", "_blank");
         },
       },{
       id: 'light-theme',
